5557dcf8b399a8ad29fd1c9f812ffc786d1bd87e
Laura <lauratelqiu@gmail.com>
2022-09-19 13:18:20 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,58 @@ import React, { useState } from 'react'
 import Expenses from './components/Expenses/Expenses';
 import { NewExpense } from './components/NewExpense/NewExpense';
 
+
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Rent",
-    amount: 2000,
-    date: new Date(2022, 7, 14),
+    title: "New Phone",
+    amount: 800,
+    date: new Date(2022, 1, 14),
+  }, {
+    id: "e2",
+    title: "Car Insurance",
+    amount: 930,
+    date: new Date(2022, 2, 15),
   },
   {
-    id: "e2",
+    id: "e3",
+    title: "Protection Plan",
+    amount: 200,
+    date: new Date(2022, 3, 14),
+  },
+  {
+    id: "e4",
+    title: "Headphones",
+    amount: 150,
+    date: new Date(2022, 5, 14),
+  },
+
+  {
+    id: "e5",
+    title: "Speakers",
+    amount: 599.99,
+    date: new Date(2022, 7, 12),
+  },
+  {
+    id: "e6",
+    title: "Keyboard",
+    amount: 230.79,
+    date: new Date(2022, 9, 12),
+  },
+  {
+    id: "e7",
     title: "New TV",
     amount: 799.49,
     date: new Date(2021, 2, 12),
   },
   {
-    id: "e3",
+    id: "e8",
     title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
-    id: "e4",
+    id: "e9",
     title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
